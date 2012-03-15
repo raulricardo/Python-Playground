0255f6cc59e8db3d81813f0ba630202dbe5a5a59
Raúl Ricardo Amaya <raulricardo21+oneiric64@gmail.com>
2012-03-14 23:17:42 -0500

    meh

---
+++ python3/bite.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Mi hola mundo, blaba bla bla
+Las triple comillas me permiten hacer comentario multi-linea
+see!
+"""
+
+def helloworld():
+    a = __name__
+    return "goodbye WOOOOORLDzu ",a
+
+print helloworld()
+
+def fib(n):
+    print 'n = ', n
+    if n > 1:
+        return n * fib(n-1)
+    else:
+        print 'end of the line'
+        return 1
+
